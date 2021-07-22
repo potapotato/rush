6e7aa831c5f249bc18e32aaee4fce1672f48ed75
waxijiang <1158612615@qq.com>
2021-07-22 09:27:25 +0800

    [新功能]: 完善了课程页面的跳转

---
+++ src/main/java/top/waxijiang/rush/controller/CourseController.java
@@ -0,0 +1,28 @@
+package top.waxijiang.rush.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import top.waxijiang.rush.service.CourseService;
+
+/**
+ * @author waxijiang
+ */
+@Controller
+@RequestMapping("/course")
+public class CourseController {
+    final
+    CourseService courseService;
+
+    public CourseController(CourseService courseService) {
+        this.courseService = courseService;
+    }
+
+    @RequestMapping("/{id}")
+    public String toCourseDetail(@PathVariable("id") String cid, Model model) {
+        model.addAttribute("course", courseService.findCourseById(cid));
+        return "course_detail";
+    }
+}
